3fcd35d0863566593899dbb886d51e491b4961a5
LucasRbnc <125413068+LucasRbnc@users.noreply.github.com>
2023-08-25 20:55:55 -0300

    Update ex1.12_export.ts

--- Ex1.12/ex1.12_export.ts
+++ Ex1.12/ex1.12_export.ts
@@ -1,14 +1,27 @@
-function maior_menor(a:number,b:number):string{
-    if(a == b){
-        return (`O número ${a} é igual ao número ${b}`);
-    }else{
-        if(a < b){
-            return (`O número ${b} é maior que o número ${a}`);
-        }else{
-            return (`O número ${a} é maior que o número ${b}`);
-        }
-    }
-}
-export{
-    maior_menor
+// function maior_menor(n1:number,n2:number):string{
+//     if(n1 == n2){
+//         return (`O número ${n1} é igual ao número ${n2}`);
+//     }else{
+//         if(a < b){
+//             return (`O número ${n2} é maior que o número ${n1}`);
+//         }else{
+//             return (`O número ${n1} é maior que o número ${n2}`);
+//         }
+//     }
+// }
+// export{
+//     maior_menor
+// }
+let n1 = 10;
+let n2 = 10;
+let mn = true
+switch(mn){
+    case mn == true && n1 < n2:
+        console.log("O número",n2,"é maior que",n1)
+    break
+    case mn == true && n1 > n2:
+        console.log("O número",n1,"é maior que",n2)
+    break
+    case mn == true && n1 == n2:
+        console.log("O número",n1,"é igual ao número",n2)
 }
