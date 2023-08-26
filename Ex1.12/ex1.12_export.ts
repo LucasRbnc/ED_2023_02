8efd910ed6e8e72765d29300e8091655a1059b0c
LucasRbnc <125413068+LucasRbnc@users.noreply.github.com>
2023-08-25 21:36:48 -0300

    Update ex1.12_export.ts

--- Ex1.12/ex1.12_export.ts
+++ Ex1.12/ex1.12_export.ts
@@ -1,27 +1,28 @@
-// function maior_menor(n1:number,n2:number):string{
-//     if(n1 == n2){
-//         return (`O número ${n1} é igual ao número ${n2}`);
-//     }else{
-//         if(a < b){
-//             return (`O número ${n2} é maior que o número ${n1}`);
-//         }else{
-//             return (`O número ${n1} é maior que o número ${n2}`);
-//         }
-//     }
-// }
-// export{
-//     maior_menor
-// }
-let n1 = 10;
-let n2 = 10;
-let mn = true
-switch(mn){
-    case mn == true && n1 < n2:
-        console.log("O número",n2,"é maior que",n1)
-    break
-    case mn == true && n1 > n2:
-        console.log("O número",n1,"é maior que",n2)
-    break
-    case mn == true && n1 == n2:
-        console.log("O número",n1,"é igual ao número",n2)
+// Exercicio 1.12 feito por Diogo e Lucas Roberto
+function maior_menor_ifelse(n1:number,n2:number):string{
+    if(n1 == n2){
+        return (`O número ${n1} é igual ao número ${n2}`);
+    }else{
+        if(n1 < n2){
+            return (`O número ${n2} é maior que o número ${n1}`);
+        }else{
+            return (`O número ${n1} é maior que o número ${n2}`);
+        }
+    }
+}
+function maior_menor_switch(n1:number,n2:number):string{
+    let mn = true
+    switch(mn){
+        case n1 < n2 == true:
+            return (`O número ${n2} é maior que o número ${n1}`);
+        break
+        case n1 > n2 == true:
+            return (`O número ${n1} é maior que o número ${n2}`);
+        break
+        case n1 == n2 == true:
+            return (`O número ${n1} é igual ao número ${n2}`);
+} return "";
+}
+export{
+    maior_menor_ifelse,maior_menor_switch
 }
