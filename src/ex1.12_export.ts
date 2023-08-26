b673bfb526277a4206a184787476c019dcbf2c2a
LucasRbnc <lucas.robertonascimento01@gmail.com>
2023-08-25 22:09:35 -0300

    Exercicios

--- src/ex1.12_export.ts
+++ src/ex1.12_export.ts
@@ -1,14 +1,28 @@
-function maior_menor(a:number,b:number):string{
-    if(a == b){
-        return (`O número ${a} é igual ao número ${b}`);
+// Exercicio 1.12 feito por Diogo e Lucas Roberto
+function maior_menor_ifelse(n1:number,n2:number):string{
+    if(n1 == n2){
+        return (`O número ${n1} é igual ao número ${n2}`);
     }else{
-        if(a < b){
-            return (`O número ${b} é maior que o número ${a}`);
+        if(n1 < n2){
+            return (`O número ${n2} é maior que o número ${n1}`);
         }else{
-            return (`O número ${a} é maior que o número ${b}`);
+            return (`O número ${n1} é maior que o número ${n2}`);
         }
     }
 }
+function maior_menor_switch(n1:number,n2:number):string{
+    let mn = true
+    switch(mn){
+        case n1 < n2 == true:
+            return (`O número ${n2} é maior que o número ${n1}`);
+        break
+        case n1 > n2 == true:
+            return (`O número ${n1} é maior que o número ${n2}`);
+        break
+        case n1 == n2 == true:
+            return (`O número ${n1} é igual ao número ${n2}`);
+} return "";
+}
 export{
-    maior_menor
+    maior_menor_ifelse,maior_menor_switch
 }
--- src/ex1.12_import.ts
+++ src/ex1.12_import.ts
@@ -1,3 +1,5 @@
-import { maior_menor } from "./ex1.12_export";
+// Exercicio 1.12 feito por Diogo e Lucas Roberto
+import { maior_menor_ifelse,maior_menor_switch } from "./ex1.12_export";
 
-console.log(maior_menor(8,8));
+console.log(maior_menor_ifelse(5,3));
+console.log(maior_menor_switch(10,10))
