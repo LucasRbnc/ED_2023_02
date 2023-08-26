76692d9554acab6fd5826a73f2acefb2317e9c47
LucasRbnc <125413068+LucasRbnc@users.noreply.github.com>
2023-08-25 21:37:20 -0300

    Create ex1.12_import.ts

---
+++ Ex1.12/ex1.12_import.ts
@@ -0,0 +1,5 @@
+// Exercicio 1.12 feito por Diogo e Lucas Roberto
+import { maior_menor_ifelse,maior_menor_switch } from "./ex1.12_export";
+
+console.log(maior_menor_ifelse(5,3));
+console.log(maior_menor_switch(10,10))
